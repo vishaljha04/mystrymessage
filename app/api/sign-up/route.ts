94d6eb1de0947e6cc851fb73d5e09731305261fe
Vishal Jha <vjha93011@gmail.com>
2026-01-17 17:39:37 +0530

    nodemailer and user wmail setup

--- app/api/sign-up/route.ts
+++ app/api/sign-up/route.ts
@@ -6,18 +6,95 @@ import { success } from "zod";
 
 export const POST = async (request: Request) => {
   await dbConnect();
+
   try {
-    const {username,email,password} = await request.json()
-    if(username || email || password){return Response.json({success:false,message:"All fields are required"},{status:400})}
-    const UserExist = await UserModel.findOne({email});
-    if(!UserExist){
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await UserModel.create({ username, email, password: hashedPassword });
+    const { username, email, password } = await request.json();
+
+    if (!username || !email || !password) {
+      return Response.json(
+        { success: false, message: "All fields are required" },
+        { status: 400 },
+      );
     }
-  } catch (error: any) {
-    console.error('Error registering user',error);
-    return Response.json({
-        success:false,message:"error registering user"
-    }),{status:500}
+
+    const existingUserVerfiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerfiedByUsername) {
+      return Response.json(
+        { success: false, message: "Username is already taken" },
+        { status: 400 },
+      );
+    }
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          { success: false, message: "User already exists with this email" },
+          { status: 400 },
+        );
+      }
+
+      //  Update unverified user
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      existingUserByEmail.password = hashedPassword;
+      existingUserByEmail.verifyCode = verifyCode;
+      existingUserByEmail.verifyCodeExpiry = expiryDate;
+      await existingUserByEmail.save();
+    } else {
+      // New user
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+
+    //  Send verification email
+    const emailResponse = await SendVerificationEmail(
+      email,
+      username,
+      verifyCode,
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        { success: false, message: emailResponse },
+        { status: 500 },
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please verify your email",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      { success: false, message: "Error registering user" },
+      { status: 500 },
+    );
   }
 };
+
--- app/helpers/sendVerificationEmail.tsx
+++ app/helpers/sendVerificationEmail.tsx
@@ -1,18 +1,32 @@
-import { resend } from "../lib/Resend";
-import VerificationEmail from '../../emails/email-template';
+import { transporter } from "../lib/nodemailer";
+import VerificationEmail from "../../emails/email-template";
 import { ApiResponse } from "../types/ApiResponse";
+import { render } from "@react-email/render";
 
-export const SendVerificationEmail = async (email:string,username:string,verifyCode:string):Promise<ApiResponse> => {
-    try {
-        await resend.emails.send({
-            from:'vjha93011@gmail.com',
-            to:email,
-            subject:"Verfication code for mystry message signup",
-            react:<VerificationEmail username={username} otp={verifyCode} />
-        })
-        return {success:true,messgae:"Verification Email Send successfully"}
-    } catch (emailError) {
-        console.error("Error Sending Verification Email",emailError)
-        return {success:false,messgae:"Failed to send Verification Email"}
-    }
-}
+export const SendVerificationEmail = async (
+  email: string,
+  username: string,
+  verifyCode: string,
+): Promise<ApiResponse> => {
+  try {
+    const emailHtml = render(VerificationEmail({ username, otp: verifyCode }));
+
+    await transporter.sendMail({
+      from: `"Mystery Message" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Verification code for Mystery Message signup",
+      html: emailHtml,
+    });
+
+    return {
+      success: true,
+      messgae: "Verification Email sent successfully",
+    };
+  } catch (emailError) {
+    console.error("Error Sending Verification Email", emailError);
+    return {
+      success: false,
+      messgae: "Failed to send Verification Email",
+    };
+  }
+};
